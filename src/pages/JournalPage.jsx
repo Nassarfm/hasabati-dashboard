c31168874af6bc8d16d926b4e74e4b164319a43b
Fadi <fadi.m.nassar@hotmail.com>
2026-03-27 00:49:16 +0300

    fix: force new bundle + remove periodStatus

--- src/pages/JournalPage.jsx
+++ src/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { PageHeader, DataTable, Field, toast, fmt, StatusBadge } from '../components/UI'
 import SlideOver from '../components/SlideOver'
 import api from '../api/client'
